f9c0cd7738955ee1df414153b4b984580e3c349a
RackGamesDev <yesterdiscord7@gmail.com>
2024-11-01 19:42:35 +0100

    a

--- Proyecto1/app/src/main/java/com/rack/proyecto1/activities/PrimeraActivity.kt
+++ Proyecto1/app/src/main/java/com/rack/proyecto1/activities/PrimeraActivity.kt
@@ -1,8 +1,11 @@
 package com.rack.proyecto1.activities
 
+import android.content.Intent //necesario para pasar entre pantallas
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button //clase necesaria para usar Button mas abajo
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,8 +23,19 @@ class PrimeraActivity : AppCompatActivity() {
             insets
         }
 
-        val boton1 = findViewById<Button>(R.id.boton1) //busca un elemento del xml de la vista para ponerlo en la variable, hace falta importar ese widget
+        val boton1 = findViewById<Button>(R.id.boton1) //busca un elemento del xml de la vista para ponerlo en la variable, hace falta importar ese widget (todos los elementos que vallan a interactuar con el codigo deberian tener id)
         val editText1 = findViewById<EditText>(R.id.editText1)
+        val texto1 = findViewById<TextView>(R.id.texto1)
+        boton1.setOnClickListener { //se ejecuta cuando ocurra el evento de ese elemento
+            val textoo = editText1.text.toString() //recibir el texto
+            if(textoo.isNotEmpty()){
+                Log.i("boton1", "click  ${textoo}") //hacer print con logcat
+                texto1.text = textoo //cambiar el texto del elemento
+                val intent = Intent(this, SegundaPantalla::class.java) //declara un intent para pasar a otra pantalla
+                intent.putExtra("EXTRA_TEXTO", textoo) //poner una variable para que la pueda leer la otra pantalla
+                startActivity(intent) //ejecutar el intent para pasar a otra pantalla finalmente (si le das a atras vuelve)
+            }
 
+        }
     }
 }
---
+++ Proyecto1/app/src/main/java/com/rack/proyecto1/activities/SegundaPantalla.kt
@@ -0,0 +1,28 @@
+package com.rack.proyecto1.activities
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rack.proyecto1.R
+
+class SegundaPantalla : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segunda_pantalla)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val resultado = findViewById<TextView>(R.id.resultado)
+        val texto:String = intent.extras?.getString("EXTRA_TEXTO").orEmpty() //recibir la variable del intent teniendo cuidado con los nulos (ya viene creado)
+        resultado.text = texto
+
+
+    }
+}
