5fb208b8a5047ef007570a8f1420d57495a0c1df
RackGamesDev <yesterdiscord7@gmail.com>
2024-11-10 15:24:58 +0100

    a

--- Proyecto1/app/src/main/java/com/rack/proyecto1/activities/SegundaPantalla.kt
+++ Proyecto1/app/src/main/java/com/rack/proyecto1/activities/SegundaPantalla.kt
@@ -7,9 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.rack.proyecto1.R
+import com.rack.proyecto1.cosasrecycler.ListaSealedAdapter
+import com.rack.proyecto1.cosasrecycler.ObjetoSealed
 
 class SegundaPantalla : AppCompatActivity() {
+
+    private lateinit var rv: RecyclerView
+    private lateinit var adaptador: ListaSealedAdapter
+    private val objetoss = listOf(ObjetoSealed.Objeto1, ObjetoSealed.Objeto2, ObjetoSealed.Otro)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +27,9 @@ class SegundaPantalla : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        adaptador = ListaSealedAdapter(objetoss)
+
 
         val resultado = findViewById<TextView>(R.id.resultado)
         val texto:String = intent.extras?.getString("EXTRA_TEXTO").orEmpty() //recibir la variable del intent teniendo cuidado con los nulos (ya viene creado)
--- Proyecto1/app/src/main/java/com/rack/proyecto1/subpaquete/ObjetoSealed.kt
+++
@@ -1,8 +0,0 @@
-package com.rack.proyecto1.subpaquete
-
-sealed class ObjetoSealed {
-    object Objeto1 : ObjetoSealed()
-    object Objeto2 : ObjetoSealed()
-    object Otro : ObjetoSealed()
-
-}
