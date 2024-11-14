3c668feaeafae872f62039db9c4fd74658d17d54
RackGamesDev <yesterdiscord7@gmail.com>
2024-11-14 14:34:00 +0100

    a

--- Proyecto1/app/src/main/java/com/rack/proyecto1/activities/PrimeraActivity.kt
+++ Proyecto1/app/src/main/java/com/rack/proyecto1/activities/PrimeraActivity.kt
@@ -1,10 +1,13 @@
 package com.rack.proyecto1.activities
 
+import android.app.Dialog
 import android.content.Intent //necesario para pasar entre pantallas
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button //clase necesaria para usar Button mas abajo
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -40,10 +43,11 @@ class PrimeraActivity : AppCompatActivity() {
         }
         initComponents() //llamara la funcion para inicializar las variables lateinit
 
-        val boton1 = findViewById<Button>(R.id.boton1) //busca un elemento del xml de la vista para ponerlo en la variable, hace falta importar ese widget (todos los elementos que vallan a interactuar con el codigo deberian tener id)
-        val editText1 = findViewById<EditText>(R.id.editText1)
+        val boton1: Button = findViewById<Button>(R.id.boton1) //busca un elemento del xml de la vista para ponerlo en la variable, hace falta importar ese widget (todos los elementos que vallan a interactuar con el codigo deberian tener id)
+    val editText1 = findViewById<EditText>(R.id.editText1)
         val texto1 = findViewById<TextView>(R.id.texto1)
         print(R.color.gray) //lo mismo que meterse en app/res/values/cualquier xml y recoger un valor (por ejemplo tambien de strings.xml)
+
         boton1.setOnClickListener { //se ejecuta cuando ocurra el evento de ese elemento
             val textoo = editText1.text.toString() //recibir el texto
             texto1.setTextColor(ContextCompat.getColor(this, R.color.gray)) //cambiar el color del texto, se pueden cambiar casi todas las propiedades de los componentes
@@ -56,15 +60,32 @@ class PrimeraActivity : AppCompatActivity() {
                 startActivity(intent) //ejecutar el intent para pasar a otra pantalla finalmente (si le das a atras vuelve)
             }
         }
+
         rango1.addOnChangeListener { slider, value, fromUser -> //listener con variables de entrada (en este caso cuando cambia el valor) (si una variable no se va a usar se cambia el nombre por _ )
             texto1.text = value.toString()
         }
-        val boton3 = findViewById<Button>(R.id.boton3)
+
+        val radios: RadioGroup = findViewById(R.id.radioGroup1)
+        val radioSeleccoinado: RadioButton = findViewById(radios.checkedRadioButtonId) //saber el id del radio seleccionado
+
+        val boton3: Button = findViewById<Button>(R.id.boton3)
         boton3.setOnClickListener {
             val intent = Intent(this, DinamicoActivity::class.java)
             startActivity(intent)
         }
 
+        val boton4: Button = findViewById<Button>(R.id.boton4)
+        boton4.setOnClickListener {
+            //codigo para mostrar un dialogo basado en un xml
+            val dialogo = Dialog(this)
+            dialogo.setContentView(R.layout.dialogo_prueba)
+            val botonDialogo: Button = dialogo.findViewById(R.id.aceptarDialogo1) //buscar elementos en el dialogo (porque es una view)
+            botonDialogo.setOnClickListener {
+                //dialogo.dismiss() //cerrar el dialogo
+                dialogo.hide() //ocultar el dialogo
+            }
+            dialogo.show() //mostrarlo finalmente
+        }
     }
 
 }
