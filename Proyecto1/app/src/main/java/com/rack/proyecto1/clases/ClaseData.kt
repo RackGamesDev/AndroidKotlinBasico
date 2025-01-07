859b9f29baf3d2da5aa0cd316b4dfbb243ba45f1
Rack Games Dev <137448965+RackGamesDev@users.noreply.github.com>
2025-01-07 21:35:31 +0100

    Delete Proyecto1 directory

--- Proyecto1/app/src/main/java/com/rack/proyecto1/clases/ClaseData.kt
+++
@@ -1,6 +0,0 @@
-package com.rack.proyecto1.clases
-
-//Las data class vienen ademas con funciones como equals()/hashCode(), toString(), copy(), componentN() pero el constructor no puede estar vacio
-data class ClaseData(var nombre:String, var tamagno:Int) { //Solo las funciones integradas funcionan con las propiedades del constructor (no pueden ser abstarct, open, sealed, inner)
-    var posicion:Float = 3f;
-}
