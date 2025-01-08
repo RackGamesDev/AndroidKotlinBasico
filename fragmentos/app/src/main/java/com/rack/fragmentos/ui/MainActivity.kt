43a6b99b61106b9bcddc72894f61f570325712b9
RackGamesDev <yesterdiscord7@gmail.com>
2025-01-08 10:40:47 +0100

    a

--- fragmentos/app/src/main/java/com/rack/fragmentos/ui/MainActivity.kt
+++ fragmentos/app/src/main/java/com/rack/fragmentos/ui/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.rack.fragmentos.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.rack.fragmentos.R
+import androidx.navigation.NavController
+import com.rack.fragmentos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
     }
 }
